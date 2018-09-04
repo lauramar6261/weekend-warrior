7654db4d426acec39817e5937e4d09916023d595
Laura <larmartinez2@gmail.com>
2018-09-04 11:36:22 -0700

    another way to select differences between two strings

--- 03-hamming/hamming.rb
+++ 03-hamming/hamming.rb
@@ -4,6 +4,12 @@ class Hamming
     raise ArgumentError if stringa.length != stringb.length
     count = 0
     index = 0
+    # another way:
+    # string1.chars.zip(string2.chars).select {|a, b| a != b}.count
+    # .chars creates an array of letters
+    # zip gives you an arrays of arrays # each inside array is a letter of the first word, and the first letter of the second word
+    # counting the number of times they don't match
+    # select return array when a != b is true, and count the length of that array # .count is an alias for length
     stringa.each_char do |letter|
       if letter != stringb[index]
         count += 1
