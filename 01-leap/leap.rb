f0afb0e2a39c2538ad2247c5bf4ffd1e875d7150
Laura <larmartinez2@gmail.com>
2018-09-04 09:28:42 -0700

    determine if a year is a leap year

--- 01-leap/leap.rb
+++ 01-leap/leap.rb
@@ -1,3 +1,56 @@
 def leap_year? (date)
-  return (date % 4 == 0) && !(date % 100 == 0) || (date % 400 == 0) ? true : false
+  # it does the || before the &&
+  # it will do AND and starts looking at the OR side
+  # if AND is false on the left side, it doesn't check the right side of
+  # return (date % 4 == 0) && (date % 100 != 0) || (date % 400 == 0) ? true : false
+  return (date % 4 == 0) && (date % 400 == 0) || (date % 100 != 0) ? true : false
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # divisible by 400
+#   return true
+# # divisible by 100
+#   return false
+# # divisible by 4
+#   return true
+# else
+#   return false
+
+# not divide by 4
+#   return false
+# divide by 100
+#   return false
+# divide by 100
+#   return true
+# else
+#   return false
